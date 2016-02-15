b96a56842c20b8270c632fb8347d68c87c6389cf
Paige Hetherington <herbmama@gmail.com>
2016-02-15 15:22:00 -0500

    broke into separate methods sorted array list by last name saved into Json file overrode toString to print in nicer format print HashMap

--- src/People.java
+++ src/People.java
@@ -1,6 +1,86 @@
-public class People {
-    public static void main(String[] args) {
+import jodd.json.JsonSerializer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class People {
+    public static void main(String[] args) throws IOException {
+
+        HashMap<String, ArrayList<Person>> countryPeople = new HashMap<>();
+        readFile(countryPeople);
+        //countryPeople = readFile();
+
+        for (ArrayList<Person> persons : countryPeople.values()) {
+            Collections.sort(persons);
+        }
+
+
+
+        System.out.println(countryPeople);
+
+        saveJson(countryPeople);
+
+        String s = new String();
+        for (String country : countryPeople.keySet()){
+            for (Person p : countryPeople.get(country)) {
+                s += p.formatPerson();
+            }
+        }
+        System.out.println(s);
 
     }
+
+
+    public static void saveJson(HashMap<String, ArrayList<Person>> countryPeople) throws IOException {
+        JsonSerializer s = new JsonSerializer();
+        String json = s.include("*").serialize(countryPeople);
+
+        File f = new File("People.json");
+        FileWriter fw = new FileWriter(f);
+        fw.write(json);
+        fw.close();
+
+    }
+    public static void readFile(HashMap<String, ArrayList<Person>> countryPeople) throws FileNotFoundException {
+    File f = new File("people.csv"); //read file
+    Scanner scanner = new Scanner(f);
+    scanner.nextLine(); //skip over first line(doesn't contain data
+    while (scanner.hasNext()) {
+        String line = scanner.nextLine(); // grab a line and read it;
+        String[] columns = line.split(","); //split into columns
+        Person p = new Person(Integer.valueOf(columns[0]), columns[1], columns[2], columns[3], columns[4], columns[5]);//moved values into an object
+        if (!countryPeople.containsKey(p.country)) {
+            countryPeople.put(p.country, new ArrayList<>());
+
+        }
+        countryPeople.get(p.country).add(p); //pass in person to array list
+    }
+
+    }
+//public static HashMap<String, ArrayList<Person>> readFile() throws FileNotFoundException {
+//    File f = new File("people.csv"); //read file
+//    HashMap<String, ArrayList<Person>> countryPeople = new HashMap<>();
+//    Scanner scanner = new Scanner(f);
+//    scanner.nextLine(); //skip over first line(doesn't contain data
+//    while (scanner.hasNext()) {
+//        String line = scanner.nextLine(); // grab a line and read it;
+//        String[] columns = line.split(","); //split into columns
+//        Person p = new Person(Integer.valueOf(columns[0]), columns[1], columns[2], columns[3], columns[4], columns[5]);//moved values into an object
+//        if (!countryPeople.containsKey(p.country)) {
+//            countryPeople.put(p.country, new ArrayList<>());
+//
+//        }
+//        countryPeople.get(p.country).add(p); //pass in person to array list
+//    }
+//    return countryPeople;
+//
+//}
+
+
 }
--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,68 @@
+import java.util.Comparator;
+
 /**
  * Created by vajrayogini on 2/15/16.
  */
-public class Person {
+public class Person implements Comparable {
+    int id;
+    String firstName;
+    String lastName;
+    String email;
+    String country;
+    String ipAddress;
+
+    public Person(int id, String firstName, String lastName, String email, String country, String ipAddress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.country = country;
+        this.ipAddress = ipAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", country='" + country + '\'' +
+                ", ipAddress='" + ipAddress + '\'' +
+                '}';
+    }
+    public String formatPerson(){
+        return String.format("%s, %s from %s \n", lastName, firstName, country);
+    }
+
+    @Override //automatically makes use of built in methods
+    public int compareTo(Object o) {
+        Person p = (Person) o; //from no on treat as email object. have access to three fields in email class
+        return lastName.compareTo(p.lastName); //compare two destinations
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
 }
